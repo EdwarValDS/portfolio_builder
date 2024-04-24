9c11484864f12c5f19f2df61d42f3987b116e335
Edwar Valenzuela <138516852+EdwarValDS@users.noreply.github.com>
2024-04-23 20:21:17 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,165 @@
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+from pypfopt import risk_models
+from pypfopt import expected_returns
+from pypfopt.efficient_frontier import EfficientFrontier
+import plotly.graph_objects as go
+from datetime import datetime, timedelta, date
+from pypfopt import plotting
+from pandas.tseries.offsets import MonthEnd, YearEnd, Week
+
+import warnings
+
+from functions import *
+warnings.filterwarnings("ignore")
+
+import streamlit as st
+
+st.title("Advanced Portfolio Builder")
+
+st.header("Initial Portfolio Analysis")
+
+
+# Price data
+own_portfolio_input = st.text_input("Enter assets separated by comma (example: AAPL, BTC-USD, CL=F, GC=F):")
+own_portfolio = [asset.strip() for asset in own_portfolio_input.split(',')]
+initial_date = st.date_input("Enter start date:", date(2023, 1, 1))
+last_date = st.date_input("Enter end date:", date(2024, 1, 1))
+
+if own_portfolio_input:
+    # Get price data
+    data = price_data(own_portfolio, initial_date, last_date, "Close")
+    correlations = data.corr()
+
+    # Calculate returns
+    returns = data.pct_change()
+
+    # Show the correlation matrix
+    st.header("Correlation Matrix")
+    st.write("Use this matrix for analyzing assets correlation")
+    sns.set_style("white")
+    plt.figure(figsize=(10, 8))
+    ax = sns.heatmap(correlations, annot=True)
+    st.pyplot(plt)  # Show the plot in Streamlit
+
+else:
+    # Wait for the user to enter assets
+    st.info("Please enter assets to continue.")
+# Portfolio backtest 
+
+st.header("Portfolio Backtest")
+
+st.markdown("### Explanation of Input Parameters:")
+st.markdown("- **Train periods:** Represents the amount of data used to optimize the portfolio.")
+st.markdown("- **Test periods:** Indicates the amount of data used to test the optimized portfolio in past iterations.")
+st.markdown("- **Periods type:** Allows you to choose between years, months, or weeks.")
+
+st.markdown("### Example:")
+st.markdown("""
+- **Start Date:** 2023-01-01
+- **End Date:** 2024-01-01
+- **Train Periods:** 3
+- **Test Periods:** 1
+- **Period Type:** Months
+
+Using this configuration, you will simulate optimizing your portfolio using the last 3 months' data for investing in the next month and rebalancing the portfolio monthly.
+""")
+
+
+train_periods = st.text_input("Enter the amount of periods for training or build the portfolio", "3")
+test_periods = st.text_input("Enter the amount of periods for testing the portfolio", "1")
+period_type = st.text_input("Enter the period you want to use: years, months or weeks")
+benchmark_asset = st.text_input("Choose a asset(ticker) as a benchmark. S&P500 is for default", "^SPX")
+
+if train_periods and test_periods and period_type and benchmark_asset:
+    train_periods = int(train_periods)
+    test_periods = int(test_periods)
+    
+    windows_train, windows_test = generate_windows(data,initial_date,last_date, train_periods, test_periods, period_type)
+    
+    profits = []
+    dates = []
+    errors = []
+    weights_results = []
+    
+    for train_window, test_window in zip(windows_train, windows_test):
+        profit, date, error, weights_result = get_results_portfolio(train_window, test_window)
+        profits.append(profit)
+        dates.append(date)
+        errors.append(error)
+        weights_results.append(weights_result)
+        
+    results = pd.DataFrame({"Date": dates, "Profit": profits, "Calc_error":errors})
+    results["Total_profit"] = results["Profit"].cumsum()
+    
+    weights_data = (round(pd.DataFrame(weights_results),2))
+    weights_data["Date"] = results["Date"]
+    
+    final_results = pd.merge(results, weights_data, on="Date")
+    final_results = final_results.set_index("Date")
+    weights_data = weights_data.set_index("Date")
+
+    
+    
+    data = price_data([benchmark_asset], initial_date,last_date, "Close")
+    data = pd.DataFrame(data)
+    data["returns"] = data["Close"].pct_change()*100
+    data = data.dropna()
+    data["benchmark_profit"] = data["returns"].cumsum()
+    
+    if period_type == "years":
+        time = "Y"
+    if period_type == "months":
+        time = "M"  
+    if period_type == "weeks":
+        time = "W"  
+    
+    data = data.resample(time).agg({"benchmark_profit":"last"})
+    data = data.reset_index()
+    final_results = final_results.reset_index()
+    
+    plot_results = pd.merge(final_results, data, on="Date")
+    plot_results = plot_results.set_index("Date")
+    
+    # Results
+    
+    st.header("Backtest results")
+    
+    st.write("Profit is measured as cumulative return of prices over time in %")
+    line_chart_st(plot_results, ["Total_profit","benchmark_profit"],"Portfolio strategy total return vs benchmark return over time")
+    
+    line_chart_st(weights_data, weights_data.columns.to_list(), "Weights over time")
+
+
+# Portfolio in real time
+
+st.header("Get your portfolio in real time")
+
+own_portfolio_input = st.text_input("Enter assets separated by comma for building your portfolio in real time:")
+own_portfolio = [asset.strip() for asset in own_portfolio_input.split(',')]
+#initial_date = st.date_input("Enter start date (just put a date bigger than your training amount):", date(2021, 1, 1))
+#last_date = st.date_input("Enter end date:", date(2024, 1, 1))
+initial_date = "2000-01-01"
+today = date.today().strftime('%Y-%m-%d')
+
+current_date = today 
+train_periods = st.text_input("Enter the amount of periods that you used in backtesting")
+#test_periods = st.text_input("Enter the amount of periods for testing the portfolio")
+period_type = st.text_input("Enter the type of period you used in backtesting")
+
+if own_portfolio_input and train_periods and period_type:
+    train_periods = int(train_periods)
+    data_realtime = price_data(own_portfolio, initial_date,today, "Close")
+    real_time_weights, train_window = get_real_time_weights(data_realtime, initial_date, current_date, train_periods, period_type)
+    
+    final_weights = pd.DataFrame(round(real_time_weights,4)*100).rename(columns={0: 'Weights in %'})
+    
+    st.header("Weights for the last moment where you had had to rebalance your portfolio")
+    st.table(final_weights)
+    st.header("Last prices data")
+    st.table(train_window)
---
+++ functions.py
@@ -0,0 +1,181 @@
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pypfopt import risk_models
+from pypfopt import expected_returns
+from pypfopt.efficient_frontier import EfficientFrontier
+import plotly.graph_objects as go
+from datetime import datetime, timedelta, date
+
+import streamlit as st
+import plotly.express as px
+from pypfopt import plotting
+
+from pandas.tseries.offsets import MonthEnd, YearEnd, Week
+
+def price_data(tickers, start_date, end_date, column):
+
+    price = yf.download(tickers, start=start_date, end=end_date)[column]
+    price.index = pd.to_datetime(price.index)
+    return price
+
+def line_chart(data, variables_list, title, width=1000, height=500, y_title=""):
+    fig = go.Figure()
+
+    for var in variables_list:
+        fig.add_trace(go.Scatter(x=data.index, y=data[var], mode='lines', name=var))
+
+    fig.update_layout(
+        title=title,
+        xaxis_title='Date',
+        yaxis_title=y_title,
+        template='plotly_dark',
+        height=height,
+        width=width
+    )
+
+    fig.show()
+
+def line_chart_st(data, variables_list, title, width=1000, height=500, y_title=""):
+    fig = px.line()
+
+    for var in variables_list:
+        fig.add_scatter(x=data.index, y=data[var], mode='lines', name=var)
+
+    fig.update_layout(
+        title=title,
+        xaxis_title='Date',
+        yaxis_title=y_title,
+        template='plotly_dark',
+        height=height,
+        width=width
+    )
+
+    st.plotly_chart(fig)
+
+def max_drawdown(columns):
+    max_profit = columns.expanding().max()  
+    drawdown = (max_profit - columns)  
+    max_drawdown = drawdown.max()  
+    return max_drawdown
+
+def generate_windows(data, initial_date, last_date, train_periods, test_periods, period_type="months"):
+    windows_train = []
+    windows_test = []
+    start_date = pd.to_datetime(initial_date)
+    end_date = pd.to_datetime(last_date)
+    
+    while True:
+        if period_type == "weeks":
+            train_end = start_date + pd.DateOffset(weeks=train_periods)
+            test_end = train_end + pd.DateOffset(weeks=test_periods)
+        elif period_type == "months":
+            train_end = start_date + pd.DateOffset(months=train_periods)
+            test_end = train_end + pd.DateOffset(months=test_periods)
+        elif period_type == "years":
+            train_end = start_date + pd.DateOffset(years=train_periods)
+            test_end = train_end + pd.DateOffset(years=test_periods)
+        else:
+            raise ValueError("Period type must be 'weeks', 'months', or 'years'")
+        
+        if test_end > end_date:
+            break
+        
+        train_window = data.loc[(data.index >= start_date) & (data.index < train_end)]
+        test_window = data.loc[(data.index >= train_end) & (data.index < test_end)]
+        
+        windows_train.append(train_window)
+        windows_test.append(test_window)
+        
+        if period_type == "weeks":
+            start_date += pd.DateOffset(weeks=1)
+        elif period_type == "months":
+            start_date += pd.DateOffset(months=1)
+        elif period_type == "years":
+            start_date += pd.DateOffset(years=1)
+    
+    return windows_train, windows_test
+
+def get_results_portfolio(train_window, test_window):
+
+    mu = expected_returns.mean_historical_return(train_window)
+    sigma = risk_models.sample_cov(train_window)
+    
+    ef = EfficientFrontier(mu, sigma)
+
+    try:
+        weights = ef.max_sharpe()
+        weights_array = pd.Series(weights)
+    except Exception as e:
+        # Use equal weights in error case
+        try:
+            weights = [1 / len(mu)] * len(mu)
+            weights_array = pd.Series(weights, index=mu.index)
+        except:
+            # If weights cannot be calculated, set all weights to 0
+            weights_array = pd.Series([0] * len(mu), index=mu.index)
+
+    returns = test_window.pct_change()
+    
+    initial_value = 10000
+    
+    try:
+        returns["pf_returns"] = returns.dot(weights_array) * 100
+        returns = round(returns, 2)
+        
+        profit = returns["pf_returns"].sum()
+        date_result = returns.index[-1]
+        error = False
+    except:
+        profit = 0
+        date_result = returns.index[-1]
+        error = True
+
+    return profit, date_result, error, weights_array
+
+def get_real_time_weights(data, initial_date, current_date, train_periods, period_type="months"):
+    start_date = pd.to_datetime(initial_date)
+    end_date = pd.to_datetime(current_date)
+    
+    while True:
+        if period_type == "weeks":
+            train_end = start_date + pd.DateOffset(weeks=train_periods)
+        elif period_type == "months":
+            train_end = start_date + pd.DateOffset(months=train_periods)
+        elif period_type == "years":
+            train_end = start_date + pd.DateOffset(years=train_periods)
+        else:
+            raise ValueError("Period type must be 'weeks', 'months', or 'years'")
+        
+        if train_end > end_date:
+            break
+        
+        train_window = data.loc[(data.index >= start_date) & (data.index < train_end)]
+        
+        mu = expected_returns.mean_historical_return(train_window)
+        sigma = risk_models.sample_cov(train_window)
+        
+        ef = EfficientFrontier(mu, sigma)
+
+        try:
+            weights = ef.max_sharpe()
+            weights_array = pd.Series(weights)
+        except Exception as e:
+            # Use equal weights in error case
+            try:
+                weights = [1 / len(mu)] * len(mu)
+                weights_array = pd.Series(weights, index=mu.index)
+            except:
+                # If weights cannot be calculated, set all weights to 0
+                weights_array = pd.Series([0] * len(mu), index=mu.index)
+        
+        if period_type == "weeks":
+            start_date += pd.DateOffset(weeks=1)
+        elif period_type == "months":
+            start_date += pd.DateOffset(months=1)
+        elif period_type == "years":
+            start_date += pd.DateOffset(years=1)
+    
+    return weights_array, train_window
